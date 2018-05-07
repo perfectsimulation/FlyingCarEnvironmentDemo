06d8bfc9f51406a6e5c4693e9055d2ed16ce1011
Sally Blair <sallyblair88@gmail.com>
2018-05-06 17:54:48 -0700

    Initial Commit. Terrain Generator.

---
+++ Assets/Editor/MapGeneratorEditor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEditor;
+using UnityEngine;
+
+// This class contains an override method for OnInspectorGUI to make changes visible without entering playmode
+[CustomEditor (typeof (MapGenerator))]
+public class MapGeneratorEditor : Editor {
+
+	public override void OnInspectorGUI () {
+		MapGenerator mapGen = (MapGenerator)target;
+
+		if (DrawDefaultInspector ()) {
+			if (mapGen.autoUpdate) {
+				mapGen.DrawMapInEditor ();
+			}
+		}
+
+		if (GUILayout.Button ("Generate")) {
+			mapGen.DrawMapInEditor ();
+		}
+	}
+}
---
+++ Assets/Scripts/HideOnPlay.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using UnityEngine;
+
+// This class is used to hide editor preview objects when entering playmode.
+public class HideOnPlay : MonoBehaviour {
+
+	void Start () {
+		gameObject.SetActive (false);
+	}
+
+}
